4cee7e2490f93ccf21943940c4c4e57a3152b7fa
Arghyajyoti Samui <108090661+Arghyajyoti007@users.noreply.github.com>
2026-01-09 23:15:52 +0530

    Add query for maximum total earnings of employees
    Add SQL query to calculate maximum total earnings and count of employees with maximum earnings.

---
+++ HackerRank/Top Earners.sql
@@ -0,0 +1,11 @@
+/*
+Enter your query here.
+*/
+-- We define an employee's total earnings to be their monthly  worked, and the maximum total earnings to be the maximum total earnings for any employee in the Employee table. Write a query to find the maximum total earnings for all employees as well as the total number of employees who have maximum total earnings. Then print these values as  space-separated integers.
+-- https://www.hackerrank.com/challenges/earnings-of-employees/problem?isFullScreen=true
+
+SELECT (salary * months) AS Earnings, COUNT(*)
+FROM Employee 
+GROUP BY Earnings
+ORDER BY Earnings DESC
+LIMIT 1;
