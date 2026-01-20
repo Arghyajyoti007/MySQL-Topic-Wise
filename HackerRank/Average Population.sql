e1b0835707546ee3d33ec1ec8ad94ae2f26e07a2
Arghyajyoti Samui <108090661+Arghyajyoti007@users.noreply.github.com>
2026-01-20 23:11:21 +0530

    Create Average Population.sql

---
+++ HackerRank/Average Population.sql
@@ -0,0 +1,10 @@
+/*
+https://www.hackerrank.com/challenges/average-population/problem?isFullScreen=true
+Query the average population for all cities in CITY, rounded down to the nearest integer.
+*/
+
+
+
+SELECT 
+    ROUND(AVG(POPULATION))
+FROM CITY;
