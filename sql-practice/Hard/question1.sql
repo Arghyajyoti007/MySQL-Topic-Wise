9019b42aff4adf7009c8f57b72e1e5dbfce38706
Arghyajyoti Samui <108090661+Arghyajyoti007@users.noreply.github.com>
2026-05-18 22:43:49 +0530

    Add SQL query for patient weight grouping
    Query to show patients grouped by weight ranges and count.

---
+++ sql-practice/Hard/question1.sql
@@ -0,0 +1,10 @@
+/*Show all of the patients grouped into weight groups.
+Show the total amount of patients in each weight group.
+Order the list by the weight group decending.
+
+For example, if they weight 100 to 109 they are placed in the 100 weight group, 110-119 = 110 weight group, etc.*/
+SELECT
+	count(*), (weight/10)*10
+FROM patients
+group by (weight/10)*10 
+order by weight desc
