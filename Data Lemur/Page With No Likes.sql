3ddcb232bf49a638a53269f4ac586eb88c6636a8
Arghyajyoti Samui <108090661+Arghyajyoti007@users.noreply.github.com>
2026-05-25 23:46:17 +0530

    Create Page With No Likes.sql

---
+++ Data Lemur/Page With No Likes.sql
@@ -0,0 +1,12 @@
+-- https://datalemur.com/questions/sql-page-with-no-likes
+/*Assume you're given two tables containing data about Facebook Pages and their respective likes (as in "Like a Facebook Page").
+
+Write a query to return the IDs of the Facebook pages that have zero likes. The output should be sorted in ascending order based on the page IDs.*/
+SELECT 
+p.page_id
+FROM pages p 
+left join page_likes pl 
+ON p.page_id = pl.page_id
+WHERE pl.page_id is null
+order by p.page_id asc
+;
