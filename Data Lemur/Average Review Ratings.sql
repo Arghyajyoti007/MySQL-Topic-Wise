fb340dfd06df883f425f324bdf44c91d94f2eb2b
Arghyajyoti Samui <108090661+Arghyajyoti007@users.noreply.github.com>
2026-05-27 22:15:45 +0530

    Create Average Review Ratings SQL query
    Add SQL query to calculate average star ratings by month.

---
+++ Data Lemur/Average Review Ratings.sql
@@ -0,0 +1,9 @@
+-- https://datalemur.com/questions/sql-avg-review-ratings
+-- Given the reviews table, write a query to retrieve the average star rating for each product, grouped by month. The output should display the month as a numerical value, product ID, and average star rating rounded to two decimal places. Sort the output first by month and then by product ID.
+SELECT 
+  extract(month from submit_date) as mth,
+  product_id, 
+  round(avg(stars), 2) as avg_stars
+FROM reviews
+group by mth, product_id
+order by mth, product_id ;
