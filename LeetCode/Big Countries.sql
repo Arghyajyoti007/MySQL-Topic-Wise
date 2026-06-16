05f1268d31d28ba5d4748eecbae94ace233a6f7e
Arghyajyoti Samui <108090661+Arghyajyoti007@users.noreply.github.com>
2026-06-16 23:56:09 +0530

    Update Big Countries.sql

--- LeetCode/Big Countries.sql
+++ LeetCode/Big Countries.sql
@@ -23,4 +23,5 @@
 -- The result format is in the following example.
 
 SELECT name, population, area FROM World
-WHERE area >= 3000000 OR population >=25000000;   
+WHERE area >= 3000000 OR population >=25000000;  
+-- SOLUTION FOR RESPECTIVE QUESTION
