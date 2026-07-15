c4da1944b545a002a2054dab87cde309605cd029
Arghyajyoti Samui <108090661+Arghyajyoti007@users.noreply.github.com>
2026-07-15 23:49:24 +0530

    Update Product Price at a Given Date.sql

--- LeetCode/Product Price at a Given Date.sql
+++ LeetCode/Product Price at a Given Date.sql
@@ -57,4 +57,19 @@ unique_products as (
 select u.product_id, COALESCE(r.price, 10) as price from price_ranks r
 right join unique_products u
 on r.product_id = u.product_id
-where r.rank_price = 1 or r.rank_price is null
+where r.rank_price = 1 or r.rank_price is null;
+
+-- UNION ALL Approach (OPTIMIZED)
+SELECT product_id, new_price AS price
+FROM Products
+WHERE (product_id, change_date) IN (
+    SELECT product_id, MAX(change_date)
+    FROM Products
+    WHERE change_date <= '2019-08-16'
+    GROUP BY product_id
+)
+UNION ALL
+SELECT product_id, 10 AS price
+FROM Products
+GROUP BY product_id
+HAVING MIN(change_date) > '2019-08-16'
